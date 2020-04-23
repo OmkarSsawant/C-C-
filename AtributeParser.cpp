8546557e37da6277054d030c3bdbe351f6850ea3
Omkar Sawant <53374390+OmkarSsawant@users.noreply.github.com>
2020-04-23 14:13:00 +0530

    victory ! ✌

--- AtributeParser.cpp
+++ AtributeParser.cpp
@@ -5,33 +5,24 @@
 
 using namespace std;
 
-bool contains(string query, map<string, string> attrm)
-{
-    map<string, string>::iterator mi = attrm.begin();
 
-    for (mi; mi != attrm.end(); mi++)
-    {
-        if (mi->first == query)
-            return true;
-    }
-
-    return false;
-}
 
 int main()
 {
 
-    int hrmli = 0, qt = 0;
+  int hrmli = 0, qt = 0;
 
-    cin >> hrmli >> qt;
+  cin >> hrmli >> qt;
 
+  if (1 <= (qt && hrmli) <= 20)
+  {
     string hrml = "";
 
     for (int i = 0; i <= hrmli; i++)
     {
-        string t;
-        getline(cin, t);
-        hrml.append(t + " ");
+      string t;
+      getline(cin, t);
+      hrml.append(t + " ");
     }
 
     string sepstr = "";
@@ -50,83 +41,124 @@ int main()
 
     while (getline(ss, sepstr, ' '))
     {
-        if (sepstr.length() > 0)
+      if (sepstr.length() > 0)
+      {
+        if (sepstr.at(0) == '<' && sepstr.at(1) != '/')
         {
-            if (sepstr.at(0) == '<' && sepstr.at(1) != '/')
-            {
-                //its an opening sepstr
-                prevTags[++i] = sepstr.substr(1);
-            }
-            else if (sepstr.substr(0, 2) == "</")
-            {
-                //its an closing sepstr
-                string clTag = sepstr.substr(2, sepstr.length() - 3);
-
-                //remove the opening tag from array
-                for (string *p = prevTags; p != lep; p++)
-                {
-                    if (*prevTags == "")
-                        break;
-                    if (*prevTags == clTag)
-                    {
-                        delete prevTags;
-                        //get back the assign index
-                        if (i != -1)
-                            i--;
-                    }
-                }
-            }
-            else if (sepstr.at(0) == 34)
-            {
-                //its an value
-                //remove *
-                sepstr.pop_back();
-                sepstr.pop_back();
-                sepstr = sepstr.substr(1);
-                if (attr != "")
-                {
-                    string key = "";
-                    for (string *itr = prevTags; itr != lep; itr++)
-                    {
-                        if (*itr == "")
-                            break;
-
-                        if (key == "")
-                            key.append(*itr);
-                        else
-                        {
-                            key.append("." + *itr);
-                        }
-                    }
-                    key += "~" + attr;
-                    attrv.insert(make_pair(key, sepstr));
-                    attr = "";
-                }
-            }
-            else
-            {
-                //its an attribute
-                if (sepstr != "=" && sepstr != ">")
-                {
-                    attr = sepstr;
-                }
-            }
+          //its an opening sepstr
+          //remove if there is closing tag continuous
+          int loclose = sepstr.find('>');
+          if (loclose > 0)
+          {
+            auto ref = sepstr.erase(loclose);
+          }
+          prevTags[++i] = sepstr.substr(1);
+          // cout<<"Opening Tag : "<<sepstr.substr(1)<<endl;
         }
-    }
+        else if (sepstr.substr(0, 2) == "</")
+        {
+          //its an closing sepstr
+          string clTag = sepstr.substr(2, sepstr.length() - 3);
 
 
-    for (int i = 0; i < qt; i++)
-    {
-        string query;
-        cin >> query;
-        if (contains(query, attrv))
-            cout << attrv.at(query) << endl;
+          //remove the opening tag from array
+          for (string *p = prevTags; p != lep; p++)
+          {
+            if (*p == "")
+              continue;
+
+            if (*p == clTag)
+            {
+              *p = "";
+              // delete p; //! TODO: not for net
+              //get back the assign index
+              if (i != -1)
+                i--;
+            }
+          }
+        }
+        else if (sepstr.at(0) == 34)
+        {
+          //its an value
+          //remove *
+          sepstr = sepstr.substr(1);
+          sepstr.erase(sepstr.find_last_of('"'));
+
+          // cout << "Value : " << sepstr << endl;
+          if (attr != "")
+          {
+            string key = "";
+            for (string *itr = prevTags; itr != lep; itr++)
+            {
+              if (*itr == "")
+                continue;
+
+              if (key == "")
+                key.append(*itr);
+              else
+              {
+                key.append("." + *itr);
+              }
+            }
+            key += "~" + attr;
+
+            //full query
+            attrv.insert(make_pair(key, sepstr));
+           
+            attr = "";
+          }
+        }
         else
-            cout << "Not Found!" << endl;
+        {
+          //its an attribute
+          if (sepstr != "=")
+          {
+            attr = sepstr;
+            // cout << "Attributes  : " << attr << endl;
+          }
+        }
+      }
     }
 
+    for (int it = 0; it < qt; it++)
+    {
+      string query;
+      cin >> query;
+      if (attrv.find(query) == attrv.end())
+        cout << "Not Found!" << endl;
+      else
+        cout << attrv.at(query) << endl;
+    }
     //free up the allocated array memory
-    delete[] prevTags;
+    //! TODO:  remove during HackerRank
+    // delete[] prevTags;
+  }
 
-    return 0;
+  return 0;
 }
+
+//just a sinnipet with which you can create sublinks 
+//eg . tag1.tag2.tag3~attr can be also called tag1.tag3~attr
+// ⚠️ caution not to use in submission code  
+
+/*
+           int ldotIndex = key.find_last_of('.');
+
+            if (ldotIndex > 0)
+            {
+
+               has parents
+              string mainq = key.substr(ldotIndex).substr(1);
+
+              string evalstr = key.substr(0, key.length() - mainq.length());
+
+              stringstream sst(evalstr);
+              string ext;
+              cout << "SubRoutes   ... " << endl;
+              while (getline(sst, ext, '.'))
+              {
+                attrv.insert(make_pair(ext + "." + mainq, sepstr));
+                cout << ext + "." + mainq << "  " << sepstr << endl;
+              }
+            }
+ */
